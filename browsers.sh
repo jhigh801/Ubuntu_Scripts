1756ad5d2b43b8e1fe806874b4bdde6995b84b96
Jon Highley <j_highley@yahoo.com>
2019-09-17 18:28:18 -0400

    Create browsers.sh

---
+++ browsers.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo apt get -y firefox chromium-browser flashplugin-installer pepperflashplugin-nonfree elinks elinks-data lynx links w3m w3m-img atool aria2c highlight highlight-common caca-utils ra
+nger
