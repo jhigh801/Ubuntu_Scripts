6011f043267fe6d74256940f06f923d4dbce2b09
Jon Highley <j_highley@yahoo.com>
2020-12-19 18:30:39 -0500

    Update browsers.sh

--- browsers.sh
+++ browsers.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo apt get -y firefox chromium-browser flashplugin-installer pepperflashplugin-nonfree elinks elinks-data lynx links w3m w3m-img atool aria2c highlight highlight-common caca-utils ra
-nger
+sudo apt get -y install firefox elinks elinks-data lynx links w3m w3m-img atool aria2c highlight highlight-common caca-utils ra
+nger 
